72ed088347ddaaabb506c7bd050852cea557cdf4
anik266 <anik_266@yahoo.com>
2015-09-12 16:54:21 -0400

    Left right works
    Direction messages are being sent.

--- web_template/public/js/controllerManager.js
+++ web_template/public/js/controllerManager.js
@@ -21,62 +21,90 @@ $(document).ready(function () {
 		//switch (payload.type) {
 			//your code here
 		//}
-		
 	});
 
-	document.addEventListener('touchstart',function(e)
-				{
-					xFirst = e.touches[0].pageX;
-					yFirst = e.touches[0].pageY;
-					
-					ClickClick = true;
-					console.log("x location = " + xFirst);
-					console.log("y location = " + yFirst);
-				});
+	document.addEventListener('touchstart',function(e){
+		var imgCursor = document.getElementById('pacman_cursor');
+		var widthCursor = imgCursor.width;
+		var heightCursor = imgCursor.height;
 
-				document.addEventListener('touchmove',function(e){
-					e.preventDefault();
+		//console.log(widthCursor);
+		//console.log(heightCursor);
 
-						if(xFirst != null)
-						{
-							xCurrent = e.touches[0].pageX; 
-							yCurrent = e.touches[0].pageY;
-	
-							xdiff = xCurrent - xFirst;
-							ydiff = yCurrent - yFirst;
-							if ( xdiff > 1 || xdiff < -1 || ydiff > 1 || ydiff < -1) 
-								ClickClick = false;
-							//var angle = Math.atan2((xCurrent - xFirst),(yCurrent - yFirst)) * Math.PI;
-							var angle = (Math.atan2((xCurrent - xFirst),(yCurrent - yFirst)) * (180/Math.PI))+180;
-							console.log("angle: " + angle);
-							//conn.sendMessage({"type": "angle", "angle": angle}, 0);	//Angle of joystick1
-						}
-							//console.log("x diff = " + xdiff);
-							//console.log("y diff = " + ydiff);
-					
-				});
+		xFirst = e.touches[0].pageX;
+		yFirst = e.touches[0].pageY;
 
-				document.addEventListener('touchend',function(e)
-				{
-						if(ClickClick == true)
-						{
-							//send click info
-							console.log("tapped");
-							ClickClick = false;
-						}
-						else
-						{
-							console.log("End");
-							//conn.sendMessage({"type": "End"}, 0 );	//when joystick end4
-						}
-						
+		ClickClick = true;
+		//console.log("x location = " + xFirst);
+		//console.log("y location = " + yFirst);
+		
+		$("#pacman_cursor").css({'top': yFirst- (heightCursor/2), 'left' : xFirst- (widthCursor/2)});
+		$("#pacman_cursor").show();
+	});
 
-						xCurrent = null;
-						yCurrent = null;
-						xFirst = null;
-						yFirst = null;
-						xdiff = null;
-						ydiff = null;			
-				});
+	document.addEventListener('touchmove',function(e){
+		e.preventDefault();
+		var imgCursor = document.getElementById('pacman_cursor');
+		var widthCursor = imgCursor.width;
+		var heightCursor = imgCursor.height;
+
+		//if(xFirst != null){
+			xCurrent = e.touches[0].pageX; 
+			yCurrent = e.touches[0].pageY;
+			$("#pacman_cursor").css({'top': yCurrent-(heightCursor/2), 'left' : xCurrent-(widthCursor/2)});
+			$("#pacman_cursor").show();
+
+			xdiff = xCurrent - xFirst;
+			ydiff = yCurrent - yFirst;
+
+			if ( xdiff > 1 || xdiff < -1 || ydiff > 1 || ydiff < -1)
+				ClickClick = false;
+				
+			var angle = (Math.atan2((xCurrent - xFirst),(yCurrent - yFirst)) * (180/Math.PI))+180;
+			//console.log("angle: " + angle);
+			//conn.sendMessage({"type": "angle", "angle": angle}, 0);	//Angle of joystick1
+		//}
+		//console.log("x diff = " + xdiff);
+		
+		//console.log("current location = " + xCurrent +", " + yCurrent);
+		//console.log("y diff = " + ydiff);
+		if ((angle > 330) || (angle < 30))
+		{
+			console.log("Swiped Up");
+			conn.sendMessage({"type": "movement", "movement": "Up"}, 0);
+		}
+		else if((angle > 240) && (angle < 300))
+		{
+			console.log("Swiped Right");
+			conn.sendMessage({"type": "movement", "movement": "Right"}, 0);
+		}
+		else if ((angle > 150) && (angle < 210))
+		{
+			console.log("Swiped Down");
+			conn.sendMessage({"type": "movement", "movement": "Down"}, 0);
+		}
+		else if ((angle > 60) && (angle < 120))
+		{
+			console.log("Swiped Left");
+			conn.sendMessage({"type": "movement", "movement": "Left"}, 0);
+		}
+	});						
+
+	document.addEventListener('touchend',function(e){
+		if(ClickClick == true){
+			//send click info
+			console.log("tapped");
+			ClickClick = false;
+		}
+
+			xCurrent = null;
+			yCurrent = null;
+			xFirst = null;
+			yFirst = null;
+			xdiff = null;
+			ydiff = null;	
+
+			$("#pacman_cursor").hide();								
+	});
+				
 });
-
